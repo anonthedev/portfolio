ae47f125991dc5fd8fcbe774890f8c1247344cf6
Anon <opss59332@gmail.com>
2022-04-16 16:01:42 +0530

    Netflix clone project image change

--- pages/index.js
+++ pages/index.js
@@ -353,6 +353,10 @@ export default function Home() {
         </div>
       </div>
 
+      <div>
+        %[https://anondevblog.hashnode.dev/why-self-control-is-an-illusion]
+      </div>
+
       <footer>
         <p></p>
         <p className="text-slate-400 text-right mr-20 pb-4">
