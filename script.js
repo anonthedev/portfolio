34d42d885d7c20f27136fbba8b30885a5cf2139b
Anon <84020006+anonthedev@users.noreply.github.com>
2024-02-14 16:56:14 +0530

    added new projects

--- script.js
+++ script.js
@@ -23,42 +23,48 @@ const socialHandles = [
 
 const projectsArray = [
   {
-    title: "Youify",
-    imgSrc: "/images/youify.png",
+    title: "youify",
     desc: "A website where you can convert your YouTube playlist to Spotify and vice versa.",
-    githubLink: "#",
+    githubLink: "https://github.com/anonthedev/youify",
     demoLink: "https://youify.xyz/",
   },
   {
+    title: "3D Renderer",
+    desc: "A 3D renderer made in vanilla JS and HTML Canvas API.",
+    githubLink: "https://github.com/anonthedev/soft3Dengine",
+    demoLink: "https://twitter.com/anonthedev/status/1753402885961093391",
+  },
+  {
     title: "youdio",
-    // imgSrc: "/images/nordic-homepage-extension.png",
-    desc: "A webapp that let's you convert youtube to an online audio jukebox.",
+    desc: "A webapp that let's you convert youtube to an online audio jukebox. Supabase took it down due to no interactivity.",
     demoLink:
       "https://youdio.xyz",
   },
   {
     title: "Rush My Flight",
-    imgSrc: "/images/rushmyflight.png",
     desc: "A freelance project wesbite where you can book flights, I handled the frontend on this project. Frontend built on Next & TS. [Project got abandoned]",
     demoLink: "https://rushmyflight.com/",
   },
   {
+    title: "Particle Simulation in JS",
+    desc: "A particle simulation made in vanilla JS & HTML Canvas",
+    githubLink: "https://github.com/anonthedev/particleLife",
+    demoLink: "https://particle-simulation.vercel.app/"
+  },
+  {
     title: "Likify",
-    imgSrc: "/images/likify.png",
     desc: "A website where you can convert your liked songs collection to a shareable spotify playlist.",
     githubLink: "https://github.com/anonthedev/Likify",
     demoLink: "https://likify.vercel.app/",
   },
   {
     title: "Screen Recorder",
-    imgSrc: "/images/screen-recorder.png",
     desc: "A web based screen recorder made using webRTC API, HTML, CSS & JS.",
     githubLink: "https://github.com/anonthedev/screen-recorder",
     demoLink: "https://anon-screen-recorder.netlify.app/",
   },
   {
     title: "Blog Thumbnail Generator",
-    imgSrc: "/images/blog-thumbnail.png",
     desc: "Just a tool made to make blog thumbnails using HTML, CSS, JS (Canvas API).",
     githubLink: "https://github.com/anonthedev/blog-thumbnail-generator",
     demoLink: "https://blog-thumbnail-generator.netlify.app",
@@ -93,7 +99,7 @@ projectsArray.forEach((project, index) => {
   githubLink.target = "_blank";
   githubLink.href = project.githubLink;
   githubLink.textContent = "github";
-  githubLink.style.display =  project.githubLink ? "block" : "none"
+  githubLink.style.display = project.githubLink ? "block" : "none"
   const demoLink = document.createElement("a");
   demoLink.target = "_blank";
   demoLink.href = project.demoLink;
